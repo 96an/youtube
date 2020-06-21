c417b7e2f0f6193ab69640cf76a87ed5ffe77835
Eban <user@users-MacBook-Air.local>
2020-06-21 19:09:00 +0600

    Initial xcode project

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Eban on 21/6/20.
+//  Copyright © 2020 Eban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
